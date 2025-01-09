7ad3a3c1dbdf7c16acccb074dd143c667e4ac716
Rober Herraiz <herraiz92@gmail.com>
2025-01-09 18:39:17 +0000

    chore: UserCredentialsDataAccess test not working

---
+++ src/test/server_app/data/UserCredentialsDataAccess.test.ts
@@ -0,0 +1,49 @@
+import { DataBase } from "../../../app/server_app/data/DataBase";
+import { UserCredentialsDataAccess } from "../../../app/server_app/data/UserCredentialsDataAccess";
+import { Account } from "../../../app/server_app/model/AuthModel";
+
+const insertMock = jest.fn();
+const getByMock = jest.fn();
+
+jest.mock("../../../app/server_app/data/DataBase", () => {
+  return {
+    DataBase: jest.fn().mockImplementation(() => {
+      () => {
+        return {
+          insert: insertMock,
+          getBy: getByMock,
+        };
+      };
+    }),
+  };
+});
+
+describe("UserCredentialsDataAccest test suite", () => {
+  let sut: UserCredentialsDataAccess;
+
+  const someAccount: Account = {
+    id: "",
+    password: "somePassword",
+    userName: "someUserName",
+  };
+
+  const someId = "1234";
+
+  beforeEach(() => {
+    sut = new UserCredentialsDataAccess();
+    expect(DataBase).toHaveBeenCalledTimes(1);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Should add user and return the id", async () => {
+    insertMock.mockResolvedValueOnce(someId); // resolvedValueOnce is for async functions
+
+    const actualId = await sut.addUser(someAccount);
+
+    expect(actualId).toBe(someId);
+    expect(insertMock).toHaveBeenCalledWith(someAccount);
+  });
+});

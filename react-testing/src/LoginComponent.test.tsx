3cf060876e755aa03da0d6b3ea14ca3750023ae2
Rober Herraiz <herraiz92@gmail.com>
2025-01-14 16:31:14 +0000

    feat: added some user interaction tests

--- react-testing/src/LoginComponent.test.tsx
+++ react-testing/src/LoginComponent.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import LoginComponent from "./LoginComponent";
 
 describe("Login Component tests", () => {
@@ -41,10 +42,35 @@ describe("Login Component tests", () => {
     // eslint-disable-next-line testing-library/no-node-access
     const inputs = container.querySelectorAll("input");
     // const inputs = document.querySelectorAll("input");
-    
+
     expect(inputs).toHaveLength(3);
     expect(inputs[0].getAttribute("value")).toBe("");
     expect(inputs[1].getAttribute("value")).toBe("");
     expect(inputs[2].getAttribute("value")).toBe("Login");
+  });
+
+  test("Click login button with incomplete credentials - show required message - with fireEvent", () => {
+    const inputs = screen.getAllByTestId("input");
+    const loginButton = inputs[2];
+
+    fireEvent.click(loginButton);
+
+    const resultLabel = screen.getByTestId("resultLabel");
+
+    expect(resultLabel).toHaveTextContent("UserName and password required!");
+  });
+
+  test("Click login button with incomplete credentials - show required message - with user click", () => {
+    const inputs = screen.getAllByTestId("input");
+    const loginButton = inputs[2];
+
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      user.click(loginButton);
+    });
+
+    const resultLabel = screen.getByTestId("resultLabel");
+
+    expect(resultLabel).toHaveTextContent("UserName and password required!");
   });
 });

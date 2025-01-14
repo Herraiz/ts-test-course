b6ea4dc638b611770ddcc9fc300271c41752d431
Rober Herraiz <herraiz92@gmail.com>
2025-01-14 15:19:33 +0000

    feat: prepared simple LoginComponent for testing

--- react-testing/src/index.tsx
+++ react-testing/src/index.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App2";
+import LoginComponent from "./LoginComponent";
+import LoginService from "./services/LoginService";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+const loginService = new LoginService();
+const setToken = (token: string) => {
+  console.log(`Token set: ${token}`);
+};
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />);

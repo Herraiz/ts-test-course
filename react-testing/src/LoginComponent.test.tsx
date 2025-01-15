ad574c4af3266b99c1fb1a9cf9cba87eb6f191df
Rober Herraiz <herraiz92@gmail.com>
2025-01-15 08:40:09 +0000

    chore: finished react testing async

--- react-testing/src/LoginComponent.test.tsx
+++ react-testing/src/LoginComponent.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-node-access */
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import LoginComponent from "./LoginComponent";
@@ -39,7 +40,6 @@ describe("Login Component tests", () => {
   });
 
   test("Should render correctly - query by document query", () => {
-    // eslint-disable-next-line testing-library/no-node-access
     const inputs = container.querySelectorAll("input");
     // const inputs = document.querySelectorAll("input");
 
@@ -72,5 +72,90 @@ describe("Login Component tests", () => {
     const resultLabel = screen.getByTestId("resultLabel");
 
     expect(resultLabel).toHaveTextContent("UserName and password required!");
+  });
+
+  //  Async tests
+
+  test("Right credentials - successfull login", async () => {
+    loginServiceMock.login.mockResolvedValueOnce("1234");
+
+    const inputs = container.querySelectorAll("input");
+    expect(inputs).toHaveLength(3);
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    fireEvent.change(userNameInput, { target: { value: "user" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.click(loginButton);
+
+    // This will not work. The value is not updated because of the await login
+    // const resultLabel = screen.getByTestId("resultLabel");
+
+    // We need to use await findByTestId
+    const resultLabel = await screen.findByTestId("resultLabel");
+    expect(resultLabel).toHaveTextContent("successful login");
+  });
+
+  test("Right credentials - successfull login with user calls", async () => {
+    loginServiceMock.login.mockResolvedValueOnce("1234");
+
+    const inputs = container.querySelectorAll("input");
+    expect(inputs).toHaveLength(3);
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    act(() => {
+      user.click(userNameInput);
+      user.keyboard("user");
+      user.click(passwordInput);
+      user.keyboard("password");
+      user.click(loginButton);
+    });
+
+    expect(loginServiceMock.login).toHaveBeenCalledWith("user", "password");
+    const resultLabel = await screen.findByTestId("resultLabel");
+    expect(resultLabel).toHaveTextContent("successful login");
+  });
+
+  test("Wrong credentials - unsuccessfull login", async () => {
+    loginServiceMock.login.mockResolvedValueOnce(undefined);
+
+    const inputs = container.querySelectorAll("input");
+    expect(inputs).toHaveLength(3);
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    fireEvent.change(userNameInput, { target: { value: "user" } });
+    fireEvent.change(passwordInput, { target: { value: "jaja" } });
+    fireEvent.click(loginButton);
+
+    const resultLabel = await screen.findByTestId("resultLabel");
+    expect(resultLabel).toHaveTextContent("invalid credentials");
+  });
+
+  test("Wrong credentials - unsuccessfull login - solve act warnings", async () => {
+    const result = Promise.resolve(undefined);
+    loginServiceMock.login.mockResolvedValueOnce(result);
+
+    const inputs = container.querySelectorAll("input");
+    expect(inputs).toHaveLength(3);
+    const userNameInput = inputs[0];
+    const passwordInput = inputs[1];
+    const loginButton = inputs[2];
+
+    act(() => {
+      user.click(userNameInput);
+      user.keyboard("user");
+      user.click(passwordInput);
+      user.keyboard("jaja");
+      user.click(loginButton);
+    });
+
+    await result;
+    const resultLabel = await screen.findByTestId("resultLabel");
+    expect(resultLabel).toHaveTextContent("invalid credentials");
   });
 });

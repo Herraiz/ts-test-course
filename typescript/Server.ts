2ad1077e28ff85f9772c0688e1438cd49fc4198f
Rober Herraiz <herraiz92@gmail.com>
2025-01-15 11:51:22 +0000

    chore: typescript init

---
+++ typescript/Server.ts
@@ -0,0 +1,45 @@
+export interface IServer {
+  startServer(): void;
+  stopServer(): void;
+}
+
+class Server implements IServer {
+  public port: number;
+  public address: string;
+  public date: string = "";
+
+  constructor(port: number, address: string) {
+    this.port = port;
+    this.address = address;
+    //    this.date = ''
+  }
+
+  async startServer() {
+    const data = await this.getData(123);
+    console.log(`Starting server at: ${this.address}: ${this.port}`);
+    return function () {
+      //    this.date = 5;
+    };
+  }
+
+  stopServer(): void {}
+
+  async getData(id: number): Promise<string> {
+    return "some SPecial Data";
+  }
+}
+
+const someServer: IServer = new Server(8080, "localhost");
+someServer.startServer();
+
+async function getToken(tokenId: string): Promise<any | undefined> {
+  return new Promise((resolve, reject) => {
+    this.nedb.find({ tokenId: tokenId }, (err: Error, docs: any[]) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(docs);
+      }
+    });
+  });
+}

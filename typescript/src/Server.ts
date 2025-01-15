e25afe32fbf789cd2ad88b2908cb88248f88921c
Rober Herraiz <herraiz92@gmail.com>
2025-01-15 12:40:41 +0000

    feat: classes and methods

--- typescript/src/Server.ts
+++ typescript/src/Server.ts
@@ -1,45 +1,16 @@
-export interface IServer {
-  startServer(): void;
-  stopServer(): void;
-}
-
-class Server implements IServer {
-  public port: number;
-  public address: string;
-  public date: string = "";
+class Server {
+  port: number;
+  addess: string;
 
   constructor(port: number, address: string) {
     this.port = port;
-    this.address = address;
-    //    this.date = ''
+    this.addess = address;
   }
 
-  async startServer() {
-    const data = await this.getData(123);
-    console.log(`Starting server at: ${this.address}: ${this.port}`);
-    return function () {
-      //    this.date = 5;
-    };
-  }
-
-  stopServer(): void {}
-
-  async getData(id: number): Promise<string> {
-    return "some SPecial Data";
+  startServer() {
+    console.log(`Server started at ${this.addess}:${this.port}`);
   }
 }
 
-const someServer: IServer = new Server(8080, "localhost");
+const someServer = new Server(3000, "localhost");
 someServer.startServer();
-
-async function getToken(tokenId: string): Promise<any | undefined> {
-  return new Promise((resolve, reject) => {
-    this.nedb.find({ tokenId: tokenId }, (err: Error, docs: any[]) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(docs);
-      }
-    });
-  });
-}

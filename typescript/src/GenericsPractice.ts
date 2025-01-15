9b2d89f6d58213fb5e3c8abab93594ac0b7ddfba
Rober Herraiz <herraiz92@gmail.com>
2025-01-15 14:25:48 +0000

    feat: added generics practice

---
+++ typescript/src/GenericsPractice.ts
@@ -0,0 +1,28 @@
+function returnKeys<T extends Object>(arg: T) {
+  console.log(Object.keys(arg));
+  return arg;
+}
+
+const a = returnKeys({ a: 1, b: 2 });
+
+interface NewPerson<T> {
+  name: string;
+  age: number;
+  special: T;
+}
+
+const john: NewPerson<string> = {
+  special: "Engineer",
+  name: "John",
+  age: 25,
+};
+
+class Observable<T extends NewPerson<string>> {
+  constructor(public value: T) {}
+
+  subscribe(arg: T): void {
+    console.log(`Subscribed to ${arg.name}`);
+  }
+}
+
+new Observable<typeof john>().subscribe(john);

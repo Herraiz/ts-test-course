be6833589807dc6fadc1e335a0734906119103af
Rober Herraiz <herraiz92@gmail.com>
2025-01-15 14:14:08 +0000

    feat: we can use interfaces instead of classes

--- typescript/src/Server.ts
+++ typescript/src/Server.ts
@@ -1,8 +1,12 @@
-abstract class BaseServer {
-  private port: number;
-  private address: string;
+interface IServer {
+  startServer(): void;
+  stopServer(): void;
+}
 
-  // we can use the constructor to initialize the properties and their access modifiers
+class Server implements IServer {
+  public port: number;
+  public address: string;
+
   constructor(port: number, address: string) {
     this.port = port;
     this.address = address;
@@ -12,25 +16,9 @@ abstract class BaseServer {
     console.log(`Server started at ${this.address}:${this.port}`);
   }
 
-  abstract stopServer(): void;
-}
-class DbServer extends BaseServer {
-  constructor(port: number, address: string) {
-    super(port, address);
-    console.log("Calling db server constructor");
-  }
-
-  // we need to implement the abstract method
-  stopServer(): void {
-    console.log("Stopping server");
-  }
+  stopServer(): void {}
 }
 
-const someServer = new DbServer(3000, "localhost");
+const someServer: IServer = new Server(3000, "localhost");
 someServer.startServer();
-
-// private override
-const somePort = (someServer as any).port;
-
-// cannot create an instance of an abstract class
-// const someOtherServer = new BaseServer(3000, "localhost");
+// we cannot access the port and address properties of someServer, because is not implemented in the interface
